8eb0ef8c0cae29116c7dc26bf3c6bb428d6872cc
Victorialalaa <75606079+Victorialalaa@users.noreply.github.com>
2021-09-13 20:09:58 -1000

    made multiplayer but laggy af

--- Assets/Scripts/MoveToClick.cs
+++ Assets/Scripts/MoveToClick.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class MoveToClick : MonoBehaviour
 {
@@ -10,32 +11,37 @@ public class MoveToClick : MonoBehaviour
     private Vector3 targetPosition;
     private Camera mainCamera;
     private Vector3 position;
+    PhotonView view;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
         rb = GetComponent<Rigidbody2D>();
+        view = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(1))//right click
+        if(view.IsMine)
         {
-            CalculateTargetPosition();
-            Debug.Log(targetPosition);
-            animator.SetBool("isRunning", true);
-        }
+            if (Input.GetMouseButtonDown(1))//right click
+            {
+                CalculateTargetPosition();
+                Debug.Log(targetPosition);
+                animator.SetBool("isRunning", true);
+            }
 
-        //locating Player position
-        GameObject player = GameObject.Find("Player");//defines player object
-        Transform playerTransform = player.transform;//player.transform command in variable
-        position = playerTransform.position;//defines players position
+            //locating Player position
+            GameObject player = GameObject.Find("Player");//defines player object
+            Transform playerTransform = player.transform;//player.transform command in variable
+            position = playerTransform.position;//defines players position
 
-        if (targetPosition == position)
-        {
-            animator.SetBool("isRunning", false);
+            if (targetPosition == position)
+            {
+                animator.SetBool("isRunning", false);
+            }
         }
     }
 
